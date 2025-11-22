a047c5ee0f33dfcf297ec183f5787571695d7a0e
HET <shuklahet2704@gmail.com>
2025-11-22 22:49:14 +0530

    resolve all tsconfig errors

--- src/db/prisma.db.ts
+++ src/db/prisma.db.ts
@@ -2,6 +2,6 @@ import { PrismaClient } from "../generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { config } from '../config/env.config';
 
-export const prismaClient = new PrismaClient();
+//export const prismaClient = new PrismaClient();
 
 
