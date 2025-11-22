4050ad3d4fc53cf506de400ab56f04f2f59392a6
HET <shuklahet2704@gmail.com>
2025-11-22 22:35:57 +0530

    resolve import erros

--- src/db/prisma.db.ts
+++ src/db/prisma.db.ts
@@ -1,3 +1,7 @@
-import { PrismaClient } from "../generated/prisma";
+import { PrismaClient } from "../generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { config } from '../config/env.config';
+
+export const prismaClient = new PrismaClient();
 
 
