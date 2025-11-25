e99a3e04b7885be0a5b0a298deedd1fce012e001
HET <shuklahet2704@gmail.com>
2025-11-26 02:48:09 +0530

    Update auth.validators.ts

--- src/modules/auth/auth.validators.ts
+++ src/modules/auth/auth.validators.ts
@@ -10,19 +10,3 @@ export const authSchema = z.object({
 });
 
 export type AuthSchema = z.infer<typeof authSchema>;
-
-
-
-
-// import { z } from "zod";
-// import { ProtectionMethod } from "@prisma/client"; // enum from Prisma
-
-// export const createShortUrlSchema = z.object({
-//   longUrl: z.string().url("Invalid URL"),
-//   protectionMethod: z.nativeEnum(ProtectionMethod).optional(),
-//   password: z.string().min(6).optional(), // only used if method=PASSWORD
-//   expiresAt: z.string().datetime().optional(), // ISO string; convert later
-// });
-
-// // Helper type from Zod schema:
-// export type CreateShortUrlBody = z.infer<typeof createShortUrlSchema>;
